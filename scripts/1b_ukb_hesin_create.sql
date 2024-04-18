17a58866746cdd1ac766bc0e8e704d6482d4e754
Mandickel Kamtengeni <mandickel@gmail.com>
2024-04-18 12:43:22 +0100

    Creation script

--- scripts/1b_ukb_hesin_create.sql
+++ scripts/1b_ukb_hesin_create.sql
@@ -1,6 +1,6 @@
 ---Creating biob_hesin Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin (
-	eid						bigint,
+	eid					bigint,
 	ins_index				bigint,
 	dsource					varchar(10),
 	source					int,
@@ -22,24 +22,24 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin (
 	elecdate				date,
 	elecdur					int,
 	admidate				date,
-	admimeth_uni			int,
+	admimeth_uni				int,
 	admimeth				int,
-	admisorc_uni			int,
+	admisorc_uni				int,
 	admisorc				int,
 	firstreg				int,
-	classpat_uni			int,
+	classpat_uni				int,
 	classpat				int,
-	intmanag_uni			int,
+	intmanag_uni				int,
 	intmanag				int,
-	mainspef_uni			int,
+	mainspef_uni				int,
 	mainspef				varchar(10),
-	tretspef_uni			int,
+	tretspef_uni				int,
 	tretspef				varchar(10),
 	operstat				int,
 	disdate					date,
-	dismeth_uni				int,
+	dismeth_uni					int,
 	dismeth					int,
-	disdest_uni				int,
+	disdest_uni					int,
 	disdest					int,
 	carersi					int)
 	TABLESPACE pg_default;
@@ -56,30 +56,30 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_critical (
 	ccadmisorc			int,
 	ccsorcloc			int,
 	ccdisdate			date,
-	ccdisrdydate		date,
+	ccdisrdydate			date,
 	ccdisstat			int,
 	ccdisdest			int,
 	ccdisloc			int,
 	ccapcrel			int,
 	bressupdays			int,
 	aressupdays			int,
-	bcardsupdays		int,
-	acardsupdays		int,
+	bcardsupdays			int,
+	acardsupdays			int,
 	rensupdays			int,
-	neurosupdays		int,
+	neurosupdays			int,
 	gisupdays			int,
 	dermsupdays			int,
-	liversupdays		int,
+	liversupdays			int,
 	orgsupmax			int,
 	cclev2days			int,
 	cclev3days			int,
 	ccunitfun			int,
-	unitbedconfig		int)
+	unitbedconfig			int)
 	TABLESPACE pg_default;
 
 ---Creating biob_hesin_delivery Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_delivery (
-	eid					bigint,
+	eid				bigint,
 	ins_index			bigint,
 	arr_index			bigint,
 	gestat				int,
@@ -95,19 +95,19 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_delivery (
 
 ---Creating biob_hesin_diag Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_diag (
-	eid					bigint,
+	eid				bigint,
 	ins_index			bigint,
 	arr_index			bigint,
 	level				int,
 	diag_icd9			varchar(10),
-	diag_icd9_nb		varchar(10),
+	diag_icd9_nb			varchar(10),
 	diag_icd10			varchar(10),
-	diag_icd10_nb		varchar(10))
+	diag_icd10_nb			varchar(10))
 	TABLESPACE pg_default;
 
 ---Creating biob_hesin_maternity Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_maternity (
-	eid					bigint,
+	eid				bigint,
 	ins_index			bigint,
 	numbaby				int,
 	numpreg				int,
@@ -125,7 +125,7 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_maternity (
 
 ---Creating biob_hesin_oper Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_oper (
-	eid					bigint,
+	eid				bigint,
 	ins_index			bigint,
 	arr_index			bigint,
 	level				int,
@@ -140,13 +140,13 @@ CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_oper (
 
 ---Creating biob_hesin_psych Table ---------------------------------------
 CREATE TABLE IF NOT EXISTS {SOURCE_SCHEMA}.biob_hesin_psych (
-	eid					bigint,
+	eid				bigint,
 	ins_index			bigint,
 	detncat_uni			int,
 	detncat				int,
 	detndate			date,
 	mentcat				int,
-	admistat_uni		int,
+	admistat_uni			int,
 	admistat			int,
 	leglstat			int)
 	TABLESPACE pg_default;
